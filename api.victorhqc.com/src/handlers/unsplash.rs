eb7b49c5033012881ff009777d004f987f873dbb
Victor Quiroz <git@victorhqc.com>
2022-05-10 17:31:03 +0200

    feat: rough query for pictures

--- api.victorhqc.com/src/handlers/unsplash.rs
+++ api.victorhqc.com/src/handlers/unsplash.rs
@@ -1,7 +1,9 @@
+use crate::modules::unsplash::pictures::fetch_random_picture;
 use rocket::serde::{json::Json, Serialize};
 
 #[get("/unsplash/picture")]
 pub async fn get_random_picture() -> Json<UnsplashPicture> {
+    fetch_random_picture().await.unwrap();
     Json(UnsplashPicture { url: "wip".into() })
 }
 
--- api.victorhqc.com/src/main.rs
+++ api.victorhqc.com/src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate rocket;
 
+#[macro_use]
+extern crate dotenv_codegen;
+
 mod handlers;
 mod modules;
 
