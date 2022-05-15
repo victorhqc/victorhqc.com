d998c9bbd5a7ff8af15c96f0a2b58daf9545e052
Victor Quiroz <git@victorhqc.com>
2022-05-15 17:20:01 +0200

    add isMobile util

--- victorhqc.com/src/pages/home/utils.ts
+++ victorhqc.com/src/pages/home/utils.ts
@@ -1,5 +1,15 @@
+import type { UAParser } from 'ua-parser-js';
+
 export function yearsDiff(date: Date) {
   const msDiff = Date.now() - date.getTime();
   const diff = new Date(msDiff);
   return Math.abs(diff.getUTCFullYear() - 1970);
+}
+
+export function isMobile(parser: UAParser) {
+  if (parser.getDevice().type && parser.getDevice().type !== 'tablet') {
+    return true;
+  }
+
+  return false;
 }
