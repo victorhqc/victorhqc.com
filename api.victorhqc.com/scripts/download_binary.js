7d0c563e79f2d427aaece2b5edced86770955318
Victor Quiroz <git@victorhqc.com>
2022-05-16 18:31:54 +0200

    fix download script
    fix pipeline
    
    debug ci
    
    add working directory
    
    change default directory
    
    fix pipeline
    
    debug ci
    
    debug ci
    
    debug ci
    
    debug ci
    
    remove default working directory
    
    add actions checkout
    
    re-enable on tag
    
    remove debug steps
    
    debug upload file
    
    debug
    
    debug
    
    debug ci
    
    add missing env
    
    debug ci
    
    upload file
    
    change path
    
    restart server
    
    re-enable release by tag
    
    attempt to fix build
    
    install zlib 1.2.9
    
    install zlib
    
    force version 1.2.9

--- api.victorhqc.com/scripts/download_binary.js
+++ api.victorhqc.com/scripts/download_binary.js
@@ -1,5 +1,6 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
+const { Readable } = require('stream');
 
 main().catch((e) => {
   console.error(e);
@@ -8,33 +9,34 @@ main().catch((e) => {
 
 async function main() {
   const asset = await getAsset();
-  console.log("ASSET", asset);
+  console.log('ASSET', asset);
+  downloadFile(asset);
 }
 
 async function getAsset() {
   const headers = new Headers();
   headers.append(
-    "Authorization",
-    `token ${process.env.TOKEN ?? "TOKEN is not defined"}`
+    'Authorization',
+    `token ${process.env.TOKEN ?? 'TOKEN is not defined'}`
   );
 
   const response = await fetch(
-    process.env.GH_TAGS ?? "GH_TAGS is not defined",
+    process.env.GH_TAGS ?? 'GH_TAGS is not defined',
     { headers }
   );
 
   if (!response.ok) {
-    throw new Error("Failed to fetch Github Data");
+    throw new Error('Failed to fetch Github Data');
   }
 
   const json = await response.json();
 
   const asset = json.assets.find(
-    (asset) => asset.name.test(/unknown-linux-musl\.zip$/g) !== null
+    (asset) => asset.name.match(/unknown-linux-musl\.zip$/g) !== null
   );
 
   if (!asset) {
-    throw new Error("Failed to find an asset.");
+    throw new Error('Failed to find an asset.');
   }
 
   return asset;
@@ -43,17 +45,42 @@ async function getAsset() {
 async function downloadFile(asset) {
   const headers = new Headers();
   headers.append(
-    "Authorization",
-    `token ${process.env.TOKEN ?? "TOKEN is not defined"}`
+    'Authorization',
+    `token ${process.env.TOKEN ?? 'TOKEN is not defined'}`
   );
+  headers.append('Accept', 'application/octet-stream');
 
-  const file = await fetch(asset.browser_download_url, { headers });
-  const desiredPath = path.join(__dirname, "..", "..", asset.name);
+  console.log('attempting to download', asset.url);
+  const file = await fetch(asset.url, { headers });
+
+  if (!file.ok) {
+    throw new Error('Failed to get binary');
+  }
+
+  const desiredPath = path.join(__dirname, '..', 'api_victorhqc_com.zip');
 
   const fileStream = fs.createWriteStream(desiredPath);
   await new Promise((resolve, reject) => {
-    file.body.pipe(fileStream);
-    file.body.on("error", reject);
-    fileStream.on("finish", resolve);
+    const readable = toReadable(file);
+    readable.pipe(fileStream);
+    readable.on('error', reject);
+    fileStream.on('finish', resolve);
   });
+}
+
+function toReadable(response) {
+  const reader = response.body.getReader();
+  const rs = new Readable();
+
+  rs._read = async () => {
+    const result = await reader.read();
+    if (!result.done) {
+      rs.push(Buffer.from(result.value));
+    } else {
+      rs.push(null);
+      return;
+    }
+  };
+
+  return rs;
 }
