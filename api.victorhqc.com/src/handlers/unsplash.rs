85439a5952f763ba5b7b085b3d4d910dd7b83455
Victor Quiroz <git@victorhqc.com>
2022-05-17 12:39:54 +0200

    add some error handler

--- api.victorhqc.com/src/handlers/unsplash.rs
+++ api.victorhqc.com/src/handlers/unsplash.rs
@@ -1,9 +1,15 @@
-use crate::modules::unsplash::{fetch_random_picture, Picture, PicturesCache};
+use crate::modules::unsplash::{
+    fetch_random_picture, Picture, PicturesCache, UnsplashPicturesError,
+};
 use async_mutex::Mutex;
 use rocket::{
+    http::Status,
+    response,
+    response::Responder,
     serde::{json::Json, Serialize},
-    State,
+    Request, State,
 };
+use snafu::prelude::*;
 use std::sync::Arc;
 
 #[get("/unsplash/picture?<query>&<orientation>")]
@@ -11,7 +17,7 @@ pub async fn get_random_picture(
     raw_state: &State<Arc<Mutex<PicturesCache>>>,
     query: &str,
     orientation: &str,
-) -> Json<UnsplashPicture> {
+) -> Result<Json<UnsplashPicture>> {
     let mut state = raw_state.lock().await;
 
     let q = match query {
@@ -25,16 +31,18 @@ pub async fn get_random_picture(
     };
 
     if state.should_fetch(q, o) {
-        let picture = fetch_random_picture(q, o).await.unwrap();
+        let picture = fetch_random_picture(q, o)
+            .await
+            .context(PictureIssueSnafu)?;
         state.set_random_picture(picture.clone(), q, o);
 
-        Json(UnsplashPicture { picture: picture })
+        Ok(Json(UnsplashPicture { picture: picture }))
     } else {
         let picture = state.get_last_random(q, o).unwrap();
 
-        Json(UnsplashPicture {
+        Ok(Json(UnsplashPicture {
             picture: picture.clone(),
-        })
+        }))
     }
 }
 
@@ -42,4 +50,22 @@ pub async fn get_random_picture(
 #[serde(crate = "rocket::serde")]
 pub struct UnsplashPicture {
     picture: Picture,
+}
+
+pub type Result<T, E = UnsplashHandlerError> = std::result::Result<T, E>;
+
+#[derive(Debug, Snafu)]
+pub enum UnsplashHandlerError {
+    #[snafu(display("Failed to get the picture {}", source))]
+    PictureIssue { source: UnsplashPicturesError },
+}
+
+impl<'r, 'o: 'r> Responder<'r, 'o> for UnsplashHandlerError {
+    fn respond_to(self, req: &'r Request<'_>) -> response::Result<'o> {
+        println!("Something went wrong! {:?}", &self);
+
+        match self {
+            _ => Status::InternalServerError.respond_to(req),
+        }
+    }
 }
