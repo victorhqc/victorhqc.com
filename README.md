27e80d71c8440597640dd168c284c1593c8691b6
Victor Quiroz <git@victorhqc.com>
2024-05-07 22:40:11 +0200

    chore: config initialization

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # victorhqc.com
+
+## How it works
+
+This website hosts my basic information as well as my photography portfolio.
+For this 1st iteration the website is pretty simple. It consists of a FE using
+Next.js and a BE using Rocket.rs, as for the photos, those are being manually
+uploaded to an S3 Bucket and recording its location in a SQLite DB.
+
+Could this be done differently? Most likely, but I don't really want to
+over-complicate it for now. Even if the deployment needs to upload the binary
+and  SQLite DB to the server.
+
+In a future iteration I plan to have a simple backoffice to handle the S3 upload
+and photos management, but that's a maybe and definitely in the future.
+
+## Requirements
+
+- Rust >= 1.78.0
+- sqlx-cli:
+
+  ```bash
+  cargo install sqlx-cli
+  ```
--- api/src/main.rs
+++ api/src/main.rs
@@ -1,11 +1,56 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use dotenv::dotenv;
+use log::info;
+use rocket::serde::Deserialize;
+use snafu::prelude::*;
+use sqlx::sqlite::SqlitePool;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+#[rocket::main]
+async fn main() -> Result<(), Error> {
+    dotenv().ok();
+    let db_url = "sqlite://development.db";
+    let pool = SqlitePool::connect(db_url).await.context(SQLXSnafu)?;
+
+    sqlx::migrate!().run(&pool).await.context(MigrationSnafu)?;
+
+    let rocket = rocket::build();
+    let figment = rocket.figment();
+
+    let config: Config = figment.extract().expect("config");
+    let database_url: String = figment.extract_inner("database_url").expect("databaseUrl");
+
+    info!("DATABASE_URL: {}", database_url);
+
+    rocket
+        .mount("/", routes![index])
+        .launch()
+        .await
+        .context(RocketSnafu)?;
+
+    Ok(())
+}
+
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct Config {
+    database_url: String,
+}
+
+#[derive(Debug, Snafu)]
+enum Error {
+    #[snafu(display("There is a problem with the DB Initialization: {}", source))]
+    SQLX { source: sqlx::Error },
+
+    #[snafu(display("Failed to run migrations: {}", source))]
+    Migration { source: sqlx::migrate::MigrateError },
+
+    #[snafu(display("Rocket Error: {}", source))]
+    Rocket { source: rocket::Error },
 }
