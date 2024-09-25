736ac84f2af41558eec73f24e2ddcbe4ac4c1c86
Victor Quiroz <git@victorhqc.com>
2024-09-25 23:30:30 +0200

    chore: string -> id

--- api/src/graphql/queries/photo.rs
+++ api/src/graphql/queries/photo.rs
@@ -1,14 +1,14 @@
-use async_graphql::{Object, Context, Result};
+use crate::db::photos::get_photo_by_id;
 use crate::graphql::context::get_pool;
 use crate::graphql::models::Photo;
-use crate::db::photos::get_photo_by_id;
+use async_graphql::{Context, Object, Result, ID};
 
 #[derive(Default)]
 pub struct PhotoQuery;
 
 #[Object]
 impl PhotoQuery {
-    pub async fn photo(&self, ctx: &Context<'_>, id: String) -> Result<Photo> {
+    pub async fn photo(&self, ctx: &Context<'_>, id: ID) -> Result<Photo> {
         let pool = get_pool(ctx).await?;
         let photo = get_photo_by_id(pool, &id).await?;
 
