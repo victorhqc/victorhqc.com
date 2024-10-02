f02a3a277dfc2677a8e53a4e7a4961c0c6e9f9f0
Victor Quiroz <git@victorhqc.com>
2024-10-02 22:51:05 +0200

    chore: add missing requirenment

--- api/src/models/fujifilm/from_tuple/mod.rs
+++ api/src/models/fujifilm/from_tuple/mod.rs
@@ -3,5 +3,7 @@ pub mod grain_effect;
 pub trait FromTuple<F, D> {
     type Err;
 
-    fn from_tuple(tuple: (F, D)) -> Result<Self, Self::Err> where Self: Sized;
+    fn from_tuple(tuple: (F, D)) -> Result<Self, Self::Err>
+    where
+        Self: Sized;
 }
