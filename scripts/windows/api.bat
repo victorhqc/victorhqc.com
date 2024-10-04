a6cfc7c0a713756d36bf6f552a8710de0e4bcbb6
Victor Quiroz <git@victorhqc.com>
2024-10-05 01:22:55 +0200

    chore: fix api script

--- scripts/windows/api.bat
+++ scripts/windows/api.bat
@@ -2,7 +2,7 @@
 setlocal
   SET RUST_DEBUG=1
   SET RUST_LOG=api=debug,sqlx::query=debug,rocket=info
-  SET DATABASE_URL=sqlite://test.db
+  SET DATABASE_URL=sqlite:development.db
   call cargo watch -i schema.gql -x "run -p api"
 
 endlocal
