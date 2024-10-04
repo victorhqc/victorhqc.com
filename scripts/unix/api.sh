ba13ab09fe1d522d817bdb25d0894bd8da3f76ec
Victor Quiroz <git@victorhqc.com>
2024-10-05 01:31:20 +0200

    chore: change api binary name
    this makes it consistent with the core crate

--- scripts/unix/api.sh
+++ scripts/unix/api.sh
@@ -4,4 +4,4 @@ export RUST_LOG=api=debug,sqlx::query=debug,rocket=info
 export RUST_DEBUG=1
 export DATABASE_URL=sqlite:development.db
 
-cargo watch -i schema.gql -x 'run -p api'
+cargo watch -i schema.gql -x 'run -p api-victorhqc-com'
--- scripts/windows/api.bat
+++ scripts/windows/api.bat
@@ -3,6 +3,6 @@ setlocal
   SET RUST_DEBUG=1
   SET RUST_LOG=api=debug,sqlx::query=debug,rocket=info
   SET DATABASE_URL=sqlite:development.db
-  call cargo watch -i schema.gql -x "run -p api"
+  call cargo watch -i schema.gql -x "run -p api-victorhqc-com"
 
 endlocal
