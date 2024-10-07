8d42c79d105862039c256b80ccf3e5234020b7a9
Victor Quiroz <git@victorhqc.com>
2024-10-07 17:20:30 +0200

    chore: exif into i64 and f64

--- core/src/exif/mod.rs
+++ core/src/exif/mod.rs
@@ -1,3 +1,6 @@
+use once_cell::sync::Lazy;
+use regex::Regex;
+
 pub mod json;
 
 pub type Tag = String;
@@ -5,3 +8,45 @@ pub type Value = String;
 
 #[derive(Debug, Clone)]
 pub struct ExifData(pub Tag, pub Value);
+
+impl From<ExifData> for Option<i64> {
+    fn from(exif: ExifData) -> Option<i64> {
+        static RE: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"(?i:([+\-0-9]+))").unwrap());
+
+        let captures = RE.captures(&exif.1)?;
+
+        let value = if let Some(v) = captures.get(1) {
+            String::from(v.as_str())
+        } else {
+            return None;
+        };
+
+        if let Ok(v) = value.parse::<i64>() {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
+
+impl From<ExifData> for Option<f64> {
+    fn from(exif: ExifData) -> Option<f64> {
+        static RE: Lazy<Regex> =
+            Lazy::new(|| Regex::new(r"(?i:([+\-0-9.]+))").unwrap());
+
+        let captures = RE.captures(&exif.1)?;
+
+        let value = if let Some(v) = captures.get(1) {
+            String::from(v.as_str())
+        } else {
+            return None;
+        };
+
+        if let Ok(v) = value.parse::<f64>() {
+            Some(v)
+        } else {
+            None
+        }
+    }
+}
