38a3a843f05c5109e8adde85dc3bb5a3c943705f
Victor Quiroz <git@victorhqc.com>
2024-10-07 17:29:08 +0200

    chore: more traits for exif data

--- core/src/exif/mod.rs
+++ core/src/exif/mod.rs
@@ -9,10 +9,31 @@ pub type Value = String;
 #[derive(Debug, Clone)]
 pub struct ExifData(pub Tag, pub Value);
 
+pub trait FromExifData {
+    fn from_exif(data: &Vec<ExifData>) -> Option<Self>;
+}
+
+pub trait FindExifData {
+    fn find(&self, exif_tag: &str) -> Option<ExifData>;
+}
+
+impl FindExifData for Vec<ExifData> {
+    fn find(&self, exif_tag: &str) -> Option<ExifData> {
+        let found = self.iter().find(|exif| exif.0 == exif_tag);
+
+        if let Some(exif) = found {
+            let exif = exif.clone();
+
+            return Some(exif);
+        }
+
+        None
+    }
+}
+
 impl From<ExifData> for Option<i64> {
     fn from(exif: ExifData) -> Option<i64> {
-        static RE: Lazy<Regex> =
-            Lazy::new(|| Regex::new(r"(?i:([+\-0-9]+))").unwrap());
+        static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i:([+\-0-9]+))").unwrap());
 
         let captures = RE.captures(&exif.1)?;
 
@@ -32,8 +53,7 @@ impl From<ExifData> for Option<i64> {
 
 impl From<ExifData> for Option<f64> {
     fn from(exif: ExifData) -> Option<f64> {
-        static RE: Lazy<Regex> =
-            Lazy::new(|| Regex::new(r"(?i:([+\-0-9.]+))").unwrap());
+        static RE: Lazy<Regex> = Lazy::new(|| Regex::new(r"(?i:([+\-0-9.]+))").unwrap());
 
         let captures = RE.captures(&exif.1)?;
 
