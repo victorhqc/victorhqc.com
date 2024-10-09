ebc4d91d258c5dd51ba9384d9285ca830f2eb92e
Victor Quiroz <git@victorhqc.com>
2024-10-09 16:40:49 +0200

    chore:  test film simulation

--- core/src/models/fujifilm/from_exif/film_simulation.rs
+++ core/src/models/fujifilm/from_exif/film_simulation.rs
@@ -23,7 +23,7 @@ impl FromExifData for FilmSimulation {
         });
 
         let captures = RE.captures(exif.value())?;
-        
+
         debug!("FilmSimulation Captures: {:?}", captures);
 
         match captures[0].to_lowercase().as_str() {
@@ -69,5 +69,256 @@ impl FromExifData for FilmSimulation {
             "reala ace" => Some(FilmSimulation::RealaAce),
             _ => None,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::exif::ExifData;
+    use crate::models::fujifilm::FilmSimulation;
+
+    #[test]
+    fn it_parses_provia() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Provia")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProviaStandard)
+        );
+    }
+
+    #[test]
+    fn it_parses_velvia() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Velvia")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::VelviaVivid)
+        );
+    }
+
+    #[test]
+    fn it_parses_astia() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Astia")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::AstiaSoft)
+        );
+    }
+
+    #[test]
+    fn it_parses_classic_chrome() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Classic Chrome")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ClassicChrome)
+        );
+    }
+
+    #[test]
+    fn it_parses_reala_ace() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Reala Ace")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::RealaAce)
+        );
+    }
+
+    #[test]
+    fn it_parses_classic_negative() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Classic Negative")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ClassicNeg)
+        );
+    }
+
+    #[test]
+    fn it_parses_nostalgic_negative() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Nostalgic Negative")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::NostalgicNeg)
+        );
+    }
+
+    #[test]
+    fn it_parses_pro_neg_std() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg Std")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegStd)
+        );
+
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg. Std")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegStd)
+        );
+
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg. Standard")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegStd)
+        );
+
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg Standard")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegStd)
+        );
+    }
+
+    #[test]
+    fn it_parses_pro_neg_high() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg Hi")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegHi)
+        );
+
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Pro Neg. Hi")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::ProNegHi)
+        );
+    }
+
+    #[test]
+    fn it_parses_eterna() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Eterna")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::EternaCinema)
+        );
+    }
+
+    #[test]
+    fn it_parses_bleach_bypass() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Bleach Bypass")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::BleachBypass)
+        );
+    }
+
+    #[test]
+    fn it_parses_acros_standard() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Acros")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Acros {
+                filter: MonochromaticFilter::Std
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_acros_yellow() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Acros Yellow Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Acros {
+                filter: MonochromaticFilter::Yellow
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_acros_green() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Acros Green Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Acros {
+                filter: MonochromaticFilter::Green
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_acros_red() {
+        let exif: Vec<ExifData> = vec![ExifData::new("FilmMode", "Acros Red Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Acros {
+                filter: MonochromaticFilter::Red
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_monochrome_standard() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Saturation", "B&W")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Monochrome {
+                filter: MonochromaticFilter::Std
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_monochrome_standard_yellow() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Saturation", "B&W Yellow Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Monochrome {
+                filter: MonochromaticFilter::Yellow
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_monochrome_green() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Saturation", "B&W Green Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Monochrome {
+                filter: MonochromaticFilter::Green
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_monochrome_red() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Saturation", "B&W Red Filter")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Monochrome {
+                filter: MonochromaticFilter::Red
+            })
+        );
+    }
+
+    #[test]
+    fn it_parses_sepia() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Saturation", "B&W Sepia")];
+
+        assert_eq!(
+            FilmSimulation::from_exif(&exif),
+            Some(FilmSimulation::Sepia)
+        );
     }
 }
