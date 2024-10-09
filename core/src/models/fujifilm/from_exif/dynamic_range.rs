d8dc3c69c7c3c3826430e42574a8ab25d0ad7aec
Victor Quiroz <git@victorhqc.com>
2024-10-09 16:27:02 +0200

    chore: test dynamic range

--- core/src/models/fujifilm/from_exif/dynamic_range.rs
+++ core/src/models/fujifilm/from_exif/dynamic_range.rs
@@ -17,3 +17,45 @@ impl FromExifData for DynamicRange {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::exif::ExifData;
+    use crate::models::fujifilm::DynamicRange;
+
+    #[test]
+    fn it_parses_auto_dynamic_range() {
+        let exif: Vec<ExifData> = vec![ExifData::new("DynamicRangeSetting", "auto")];
+
+        assert_eq!(DynamicRange::from_exif(&exif), Some(DynamicRange::Auto));
+    }
+
+    #[test]
+    fn it_parses_dr100_dynamic_range() {
+        let exif: Vec<ExifData> = vec![ExifData::new("DynamicRangeSetting", "100")];
+
+        assert_eq!(DynamicRange::from_exif(&exif), Some(DynamicRange::DR100));
+    }
+
+    #[test]
+    fn it_parses_dr200_dynamic_range() {
+        let exif: Vec<ExifData> = vec![ExifData::new("DynamicRangeSetting", "200")];
+
+        assert_eq!(DynamicRange::from_exif(&exif), Some(DynamicRange::DR200));
+    }
+
+    #[test]
+    fn it_parses_dr400_dynamic_range() {
+        let exif: Vec<ExifData> = vec![ExifData::new("DynamicRangeSetting", "400")];
+
+        assert_eq!(DynamicRange::from_exif(&exif), Some(DynamicRange::DR400));
+    }
+
+    #[test]
+    fn it_does_not_parse_when_not_found() {
+        let exif: Vec<ExifData> = vec![ExifData::new("Foo", "3")];
+
+        assert_eq!(DynamicRange::from_exif(&exif), None);
+    }
+}
