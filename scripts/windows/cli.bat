0251b5d4058f47a6dcdd1f3767db4910593ecb69
Victor Quiroz <git@victorhqc.com>
2024-10-09 21:05:20 +0200

    chore: remove unused scripts

--- scripts/windows/cli.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-setlocal
-  SET RUST_DEBUG=1
-  SET RUST_LOG=cli_victorhqc_com=debug,sqlx::query=debug,rocket=info
-  SET DATABASE_URL=sqlite:development.db
-  call cargo run -p cli-victorhqc-com -- %*
-
-endlocal
