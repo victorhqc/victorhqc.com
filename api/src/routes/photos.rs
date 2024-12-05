f4cbb70a845a08a2219717ff100f3b76f15d9bb1
Victor Quiroz <git@victorhqc.com>
2024-12-05 21:27:34 +0100

    feat: render the home page with a random photo

--- api/src/routes/photos.rs
+++ api/src/routes/photos.rs
@@ -1,5 +1,6 @@
 use crate::AppState;
-use core_victorhqc_com::models::photo::Photo;
+use core_victorhqc_com::models::{photo::Photo, tag::Tag};
+use rand::seq::SliceRandom;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::State;
@@ -12,4 +13,18 @@ pub async fn get_all_photos(state: &State<AppState>) -> (Status, Json<Vec<Photo>
     let photos = Photo::find_all(&mut conn).await.unwrap();
 
     (Status::Ok, Json(photos))
+}
+
+#[get("/photos/<tag>/random", format = "json")]
+pub async fn get_random_photo(tag: &str, state: &State<AppState>) -> (Status, Json<Photo>) {
+    let pool = &state.db_pool;
+    let mut conn = pool.try_acquire().unwrap();
+
+    let tag = Tag::find_by_name(&mut conn, tag).await.unwrap();
+    let photos = Photo::find_by_tag_ids(&mut conn, &vec![tag.id])
+        .await
+        .unwrap();
+    let (_, random_photo) = photos.choose(&mut rand::thread_rng()).unwrap();
+
+    (Status::Ok, Json(random_photo.clone()))
 }
