9f490624f2c7d7d7044c52686798d21047f934c4
Victor Quiroz <git@victorhqc.com>
2024-12-05 23:57:56 +0100

    Better random image (#23)
    Instead of calculating a random image per request, which needs to query
    the DB in every request. The random is instead calculated in the
    frontend web-server, simply get all the `portfolio` photo ids, and then
    randomize which `id` to get on every request.
    
    This way, the amounts of queries gets decreased, which should result in
    more requests per second without overloading the SQL connections.

--- api/src/routes/photos.rs
+++ api/src/routes/photos.rs
@@ -1,6 +1,5 @@
 use crate::AppState;
 use core_victorhqc_com::models::{photo::Photo, tag::Tag};
-use rand::seq::SliceRandom;
 use rocket::http::Status;
 use rocket::serde::json::Json;
 use rocket::State;
@@ -15,16 +14,21 @@ pub async fn get_all_photos(state: &State<AppState>) -> (Status, Json<Vec<Photo>
     (Status::Ok, Json(photos))
 }
 
-#[get("/photos/<tag>/random", format = "json")]
-pub async fn get_random_photo(tag: &str, state: &State<AppState>) -> (Status, Json<Photo>) {
+#[get("/photos/<name>", format = "json")]
+pub async fn get_all_photos_by_tag(
+    name: &str,
+    state: &State<AppState>,
+) -> (Status, Json<Vec<Photo>>) {
     let pool = &state.db_pool;
     let mut conn = pool.try_acquire().unwrap();
 
-    let tag = Tag::find_by_name(&mut conn, tag).await.unwrap();
+    let tag = Tag::find_by_name(&mut conn, name).await.unwrap();
     let photos = Photo::find_by_tag_ids(&mut conn, &vec![tag.id])
         .await
-        .unwrap();
-    let (_, random_photo) = photos.choose(&mut rand::thread_rng()).unwrap();
+        .unwrap()
+        .into_iter()
+        .map(|(_, photo)| photo)
+        .collect();
 
-    (Status::Ok, Json(random_photo.clone()))
+    (Status::Ok, Json(photos))
 }
