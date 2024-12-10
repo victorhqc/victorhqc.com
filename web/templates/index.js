b6ed5e7bebc62b2e2f7acb19bd001564cf9d0e52
Victor Quiroz <git@victorhqc.com>
2024-12-10 22:38:20 +0100

    chore: wip scroll on stack

--- web/templates/index.js
+++ web/templates/index.js
@@ -1,53 +1,159 @@
-const photos = document.querySelectorAll(".photo");
-const size = photos.length;
+(function () {
+  const photos = document.querySelectorAll(".photo");
+  const stack = document.querySelector("#photos-stack");
 
-for (const [i, photo] of photos.entries()) {
-  if (i === size - 1) continue;
+  scramblePhotos(photos);
 
-  const minDeg = i * 4 + 1;
-  const maxDeg = i * 5 + 5;
-  const degrees = randomIntFromInterval(minDeg, maxDeg);
+  if (stack) {
+    console.log("Enabling swipe to", stack);
+    swipe(stack, photos);
+  }
 
-  const minX = randomPositiveNegative(30);
-  const maxX = randomPositiveNegative(50);
-  const x = randomIntFromInterval(minX, maxX);
+  /**
+   *
+   * @param {NodeListof<Element>} elements
+   */
+  function scramblePhotos(elements) {
+    const size = elements.length;
+    for (const [i, photo] of elements.entries()) {
+      if (i === size - 1) continue;
 
-  const minY = randomPositiveNegative(30);
-  const maxY = randomPositiveNegative(50);
-  const y = randomIntFromInterval(minY, maxY);
+      const minDeg = i * 4 + 1;
+      const maxDeg = i * 5 + 5;
+      const degrees = randomIntFromInterval(minDeg, maxDeg);
 
-  displaceAndRotateElement(photo, x, y, degrees);
-}
+      const minX = randomPositiveNegative(30);
+      const maxX = randomPositiveNegative(50);
+      const x = randomIntFromInterval(minX, maxX);
 
-/**
- *
- * @param {HTMLElement} el
- * @param {number} x
- * @param {number} y
- * @param {number} degrees
- */
-function displaceAndRotateElement(el, x, y, degrees) {
-  console.log(`Displacing ${x},${y} and rotating ${degrees}deg`);
-  el.style.transform = `translate(${x}px, ${y}px) rotate(${degrees}deg)`;
-}
+      const minY = randomPositiveNegative(30);
+      const maxY = randomPositiveNegative(50);
+      const y = randomIntFromInterval(minY, maxY);
 
-/**
- *
- * @param {number} min
- * @param {number} max
- * @returns
- */
-function randomIntFromInterval(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
+      displaceAndRotateElement(photo, x, y, degrees);
+    }
+  }
 
-/**
- *
- * @param {number} number
- * @returns
- */
-function randomPositiveNegative(number) {
-  const sign = Math.random() < 0.5 ? -1 : 1;
+  /**
+   *
+   * @param {HTMLElement} el
+   * @param {number} x
+   * @param {number} y
+   * @param {number} degrees
+   */
+  function displaceAndRotateElement(el, x, y, degrees) {
+    console.log(`Displacing ${x},${y} and rotating ${degrees}deg`);
+    el.style.transform = `translate(${x}px, ${y}px) rotate(${degrees}deg)`;
+  }
 
-  return number * sign;
-}
+  /**
+   *
+   * @param {number} min
+   * @param {number} max
+   * @returns
+   */
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  /**
+   *
+   * @param {number} number
+   * @returns
+   */
+  function randomPositiveNegative(number) {
+    const sign = Math.random() < 0.5 ? -1 : 1;
+
+    return number * sign;
+  }
+
+  /**
+   *
+   * @param {Element} wrapper
+   * @param {NodeListOf<Element>} photos
+   */
+  function swipe(wrapper, photos) {
+    addWheelEvent(wrapper, photos);
+
+    wrapper.addEventListener("touchstart", (event) => {
+      console.log("TOUCH START", event);
+      // startX = event.touches[0].clientX;
+    });
+
+    wrapper.addEventListener("touchmove", (event) => {
+      console.log("TOUCH MOVE", event);
+    });
+  }
+
+  /**
+   *
+   * @param {Element} wrapper
+   * @param {NodeListOf<Element>} photos
+   */
+  function addWheelEvent(wrapper, photos) {
+    const photosLength = photos.length;
+    console.log("PHOTOS LENGTH", photosLength);
+
+    const activeIndex = getIndexFromActive(photos);
+    console.log("ACTIVE INDEX", activeIndex);
+
+    let isThrottled = false;
+    wrapper.addEventListener("wheel", (event) => {
+      if (isThrottled) return;
+      console.log("WHEEL EVENT", event);
+
+      const activeIndex = getIndexFromActive(photos);
+      console.log("ACTIVE INDEX", activeIndex);
+      if (activeIndex < 0) return;
+
+      const direction = event.deltaY > 0 ? "UP" : "DOWN";
+      console.log("DIRECTION ", direction);
+
+      let nextIndexActive = -1;
+
+      switch (direction) {
+        case "UP":
+          nextIndexActive =
+            activeIndex === 0 ? photosLength - 1 : activeIndex - 1;
+          break;
+        case "DOWN":
+          nextIndexActive =
+            activeIndex === photosLength - 1 ? 0 : activeIndex + 1;
+          break;
+      }
+
+      console.log("NEW INDEX", nextIndexActive);
+
+      for (const [index, photo] of photos.entries()) {
+        if (index === activeIndex) {
+          photo.classList.remove("active");
+        }
+
+        if (index === nextIndexActive) {
+          photo.classList.add("active");
+        }
+      }
+
+      console.log("--");
+
+      isThrottled = true;
+      setTimeout(() => (isThrottled = false), 800);
+    });
+  }
+
+  /**
+   *
+   * @param {NodeListOf<Element>} photos
+   */
+  function getIndexFromActive(photos) {
+    let activeIndex = -1;
+    for (const [index, photo] of photos.entries()) {
+      if (photo.classList.contains("active")) {
+        activeIndex = index;
+        break;
+      }
+    }
+
+    return activeIndex;
+  }
+})();
