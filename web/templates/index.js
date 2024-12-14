ab48ab3d0da266111b505b08db7a2a0ebb676e56
Victor Quiroz <git@victorhqc.com>
2024-12-14 17:42:44 +0100

    feat: smooth animation

--- web/templates/index.js
+++ web/templates/index.js
@@ -1,6 +1,14 @@
 (async function () {
+  const REGEX_TRANSFORM =
+    /(translate3d\(([-+]?[0-9]+px,?\s?){3}\))\s?(rotate\([-+]?[0-9]+deg\))/gi;
+  const REGEX_TRANSLATE3D =
+    /translate3d\(((-?[0-9]+)px,?\s?)(((\+|-)?[0-9]+)px,?\s?)(((\+|-)?[0-9]+)px,?\s?)\)/gi;
+
   /**
-   * @typedef {Array.<{photo: Element, x: number, y: number, z: number, degrees: number}>} ScrambledElement
+   * @typedef {Object} ScrambledElement
+   * @property {number} degrees
+   * @property {number} x
+   *
    * @typedef {"UP" | "DOWN"} Direction
    */
 
@@ -11,11 +19,11 @@
 
   /**
    *
-   * @param {NodeListOf<Element>} photos
+   * @param {NodeListOf<HTMLElement>} photos
    * @returns
    */
   function waitForAllToLoad(photos) {
-    /** @type {Array<Element>} */
+    /** @type {Array<HTMLElement>} */
     let loaded = [];
     return new Promise(async (resolve, reject) => {
       for (const photo of photos) {
@@ -48,41 +56,32 @@
     const scrambledPhotos = calculateScramblePositions(photos);
 
     for (const { photo, x, y, z, degrees } of scrambledPhotos) {
-      displaceAndRotateElement(photo, 0, 0, 0, 0);
+      photo.style.transform = `translate3d(0px, 0px, 0px) rotate(0deg)`;
       setTimeout(() => {
-        displaceAndRotateElement(photo, x, y, z, degrees);
+        photo.style.transform = `translate3d(${x}px, ${y}px, ${z}px) rotate(${degrees}deg)`;
       }, 50);
     }
 
     const stack = document.querySelector("#photos-stack");
     if (stack) {
-      swipe(scrambledPhotos, stack, ".photo");
+      swipe(stack, ".photo", scrambledPhotos);
     }
   }
 
   /**
    *
-   * @param {NodeListOf<Element>} elements
+   * @param {NodeListOf<HTMLElement>} elements
+   * @returns {Array.<{ photo: HTMLElement, x: number, y: number, z: number, degrees: number }>}
    */
   function calculateScramblePositions(elements) {
     const last = elements.length - 1;
 
+    /** @type {Array.<{ photo: HTMLElement, x: number, y: number, z: number, degrees: number }>} */
     const scrambled = [];
     for (const [i, photo] of elements.entries()) {
-      if (i === last) {
-        scrambled.push({ photo, x: 0, y: 0, z: 0, degrees: 0 });
-        // photo.style.opacity = 0.5;
-
-        continue;
-      }
-
-      // photo.style.opacity = 0;
-
-      const x = 0;
-      const y = calculateYAxis(i, last, "DOWN");
-      const z = calculateZAxis(i, last);
-      const degrees = 0;
-      // const { x, y, z, degrees } = calculateScramblePosition(i, last);
+      const y = calculateYAxis(photo, i, last, "DOWN");
+      const z = calculateZAxis(photo, i, last, "DOWN");
+      const { degrees, x } = calculateScramblePosition(i, last);
       scrambled.push({ photo, x, y, z, degrees });
     }
 
@@ -93,36 +92,273 @@
    *
    * @param {number} index
    * @param {number} last
-   * @returns {Array<ScrambledElement>}
+   * @returns {ScrambledElement}
    */
   function calculateScramblePosition(index, last) {
+    if (index === last) {
+      return { x: 0, degrees: 0 };
+    }
+
     const minDeg = index * 4 + 1;
     const maxDeg = index * 5 + 5;
     const degrees = randomIntFromInterval(minDeg, maxDeg);
 
-    const minX = randomPositiveNegative(30);
-    const maxX = randomPositiveNegative(50);
+    const minX = randomPositiveNegative(20);
+    const maxX = randomPositiveNegative(30);
     const x = randomIntFromInterval(minX, maxX);
 
-    const minY = randomPositiveNegative(30);
-    const maxY = randomPositiveNegative(50);
-    const y = randomIntFromInterval(minY, maxY);
-
-    const z = calculateZAxis(index, last);
-
-    return { x, y, z, degrees };
+    return { degrees, x };
   }
 
   /**
    *
-   * @param {Element} el
-   * @param {number} x
-   * @param {number} y
-   * @param {number} z
-   * @param {number} degrees
+   * @param {Element} wrapper
+   * @param {string} photosSelector
+   * @param {Array<ScrambledElement>} scrambled
    */
-  function displaceAndRotateElement(el, x, y, z, degrees) {
-    el.style.transform = `translate3d(${x}px, ${y}px, ${z}px) rotate(${degrees}deg) scale(1)`;
+  function swipe(wrapper, photosSelector, scrambled) {
+    addWheelEvent(wrapper, photosSelector, scrambled);
+
+    wrapper.addEventListener("touchstart", (event) => {
+      console.log("TOUCH START", event);
+    });
+
+    wrapper.addEventListener("touchmove", (event) => {
+      console.log("TOUCH MOVE", event);
+    });
+  }
+
+  /**
+   *
+   * @param {HTMLElement} wrapper
+   * @param {string} photosSelector
+   * @param {Array<ScrambledElement>} scrambled
+   */
+  function addWheelEvent(wrapper, photosSelector, scrambled) {
+    let isThrottled = false;
+    wrapper.addEventListener("wheel", (event) => {
+      event.preventDefault();
+
+      if (isThrottled) return;
+      const photos = document.querySelectorAll(photosSelector);
+      const direction = event.deltaY > 0 ? "UP" : "DOWN";
+
+      let sortedPhotos;
+      switch (direction) {
+        case "UP":
+          sortedPhotos = sortPhotos(
+            photos,
+            (maxLength, index) => (index === 0 ? maxLength : index - 1),
+            (element, index, last) => {
+              if (index !== last) return;
+
+              const { x, degrees } = findOriginalOrThrow(scrambled, element);
+
+              // element.style.transformOrigin = "50% 100%";
+              // element.style.transformOrigin = "0 0";
+              element.style.transform = `translate3d(${x}px, 20px, -20px) rotate(${degrees}deg)`;
+              element.style.zIndex = -2;
+              element.style.transitionDuration = "150ms";
+            },
+          );
+          break;
+        case "DOWN":
+          sortedPhotos = sortPhotos(
+            photos,
+            (maxLength, index) => (index === 0 ? maxLength : index - 1),
+            (element, index, last) => {
+              if (index !== last) return;
+              const { x, degrees } = findOriginalOrThrow(scrambled, element);
+
+              // element.style.transformOrigin = "50% 0";
+              // element.style.transformOrigin = "50% 50%";
+              element.style.transform = `translate3d(${x}px, -20px, -20px) rotate(${degrees}deg)`;
+              element.style.zIndex = -2;
+              element.style.transitionDuration = "150ms";
+            },
+          );
+          break;
+      }
+
+      setTimeout(() => {
+        animateOnMovement(sortedPhotos, direction, scrambled);
+      }, 50);
+
+      isThrottled = true;
+      setTimeout(() => (isThrottled = false), 600);
+    });
+  }
+
+  /**
+   *
+   * @param {NodeListOf<HTMLElement>} elements
+   * @param {Direction} direction
+   * @param {Array<ScrambledElement>} scrambled
+   */
+  function animateOnMovement(elements, direction, scrambled) {
+    const last = elements.length - 1;
+    elements.forEach((element, index) => {
+      const newZ = calculateZAxis(element, index, last, direction);
+      const newY = calculateYAxis(element, index, last, direction);
+      const { x, degrees } = findOriginalOrThrow(scrambled, element);
+
+      // This means the photo is on top of the stack, so we want to restore the original "Y" value and reset the "Z" to 0
+      if (index === last) {
+        setTimeout(() => {
+          element.style.zIndex = 0;
+        }, 1);
+
+        setTimeout(() => {
+          element.style.transform = `translate3d(${x}px, 0px, 0px) rotate(${degrees}deg)`;
+          element.style.transformOrigin = "";
+          element.style.zIndex = 0;
+        }, 50);
+      } else {
+        element.style.transitionDuration = "300ms";
+        element.style.transform = `translate3d(${x}px, ${newY}px, ${newZ}px) rotate(${degrees}deg)`;
+      }
+
+      setTimeout(() => (element.style.transitionDuration = "0ms"), 300);
+    });
+  }
+
+  /**
+   * @callback sortPhotosCallback
+   * @param {maxLength} number
+   * @param {number} index
+   * @returns {number}
+   */
+
+  /**
+   * Executes the CB **Before** Rendering the element again in the document. Useful to apply a style on
+   * mount.
+   *
+   * @callback stylingCallback
+   * @param {HTMLElement} element
+   * @param {number} index
+   * @param {number} last
+   */
+
+  /**
+   *
+   * @param {NodeListOf<Element>} elms
+   * @param {sortPhotosCallback} sortCb
+   * @param {stylingCallback} stylingCb
+   */
+  function sortPhotos(elms, sortCb, stylingCb) {
+    if (elms.length === 0) return Array.from(elms);
+
+    const firstElement = elms[0];
+
+    const parent = firstElement.parentNode;
+    if (!parent) return Array.from(elms);
+
+    const last = elms.length - 1;
+    const elements = Array.from(elms)
+      .map((element, index) => {
+        const newIndex = sortCb(last, index);
+
+        return { element, newIndex };
+      })
+      .sort((a, b) => a.newIndex - b.newIndex)
+      .map(({ element }) => element);
+
+    elements.forEach((element, index) => {
+      element.remove();
+      stylingCb(element, index, last);
+
+      parent.appendChild(element);
+    });
+
+    return elements;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} el
+   * @param {number} index
+   * @param {number} last
+   * @param {Direction} direction
+   */
+  function calculateZAxis(el, index, last, direction) {
+    const { z } = getTransform(el);
+    const isMain = index === last;
+
+    if (isMain) {
+      return 0;
+    }
+
+    if (z === 0) {
+      return -10 * (last - index);
+    }
+
+    return z - 10;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} el
+   * @param {number} index
+   * @param {number} last
+   * @param {Direction} direction
+   */
+  function calculateYAxis(el, index, last, direction) {
+    const { y } = getTransform(el);
+    const isMain = index === last;
+
+    if (isMain) {
+      return 0;
+    }
+
+    if (direction === "DOWN") {
+      if (y === 0) {
+        return 20 * (last - index);
+      }
+
+      return y + 20;
+    }
+
+    if (y === 0) {
+      return -20 * (last - index);
+    }
+
+    return y - 20;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} element
+   */
+  function getTransform(element) {
+    const originalTransform = element.style.transform;
+
+    const matches = [...originalTransform.matchAll(REGEX_TRANSFORM)];
+    if (matches.length === 0) {
+      return { x: 0, y: 0, z: 0, rotate: `rotate(0deg)` };
+    }
+
+    const translate = matches[0][1];
+    const rotate = matches[0][3];
+
+    const translateMatches = [...translate.matchAll(REGEX_TRANSLATE3D)];
+    const x = parseInt(translateMatches[0][2]);
+    const y = parseInt(translateMatches[0][4]);
+    const z = parseInt(translateMatches[0][7]);
+
+    return { x, y, z, rotate };
+  }
+
+  /**
+   * @param {Array<ScrambledElement} scrambled
+   * @param {HTMLElement} element
+   */
+  function findOriginalOrThrow(scrambled, element) {
+    const original = scrambled.find(({ photo }) => photo === element);
+    if (!original) {
+      throw new Error("Could not find original");
+    }
+
+    return original;
   }
 
   /**
@@ -142,201 +378,7 @@
    */
   function randomPositiveNegative(number) {
     const sign = Math.random() < 0.5 ? -1 : 1;
-
     return number * sign;
-  }
-
-  /**
-   *
-   * @param {Array.<ScrambledElement>} scrambledPhotos
-   * @param {Element} wrapper
-   * @param {string} photosSelector
-   */
-  function swipe(scrambledPhotos, wrapper, photosSelector) {
-    addWheelEvent(wrapper, photosSelector, scrambledPhotos);
-
-    wrapper.addEventListener("touchstart", (event) => {
-      console.log("TOUCH START", event);
-    });
-
-    wrapper.addEventListener("touchmove", (event) => {
-      console.log("TOUCH MOVE", event);
-    });
-  }
-
-  /**
-   *
-   * @param {Element} wrapper
-   * @param {string} photosSelector
-   * @param {Array.<ScrambledElement>} scrambledPhotos
-   */
-  function addWheelEvent(wrapper, photosSelector, scrambledPhotos) {
-    let isThrottled = false;
-    wrapper.addEventListener("wheel", (event) => {
-      event.preventDefault();
-
-      if (isThrottled) return;
-      const photos = document.querySelectorAll(photosSelector);
-
-      const direction = event.deltaY > 0 ? "UP" : "DOWN";
-
-      let sortedPhotos;
-      switch (direction) {
-        case "UP":
-          sortedPhotos = sortPhotos(photos, (maxLength, index) =>
-            index === maxLength ? 0 : index + 1,
-          );
-          break;
-        case "DOWN":
-          sortedPhotos = sortPhotos(photos, (maxLength, index) =>
-            index === 0 ? maxLength : index - 1,
-          );
-          break;
-      }
-
-      setTimeout(() => {
-        animateOnMovement(sortedPhotos, direction, scrambledPhotos);
-      }, 50);
-
-      isThrottled = true;
-      setTimeout(() => (isThrottled = false), 400);
-    });
-  }
-
-  /**
-   *
-   * @param {NodeListOf<Element>} elements
-   * @param {Direction} direction
-   * @param {Array.<ScrambledElement>} scrambledPhotos
-   */
-  function animateOnMovement(elements, direction, scrambledPhotos) {
-    console.log("DIRECTION", direction);
-    const last = elements.length - 1;
-    elements.forEach((element, index) => {
-      const original = scrambledPhotos.find(({ photo }) => photo === element);
-
-      if (!original) {
-        throw new Error("Could not find originals");
-      }
-
-      const newZ = calculateZAxis(index, last, direction);
-      const newY = calculateYAxis(index, last, direction);
-
-      const { x, y, degrees } = original;
-
-      // This means the photo is on top of the stack, so we want to restore the original "Y" value and reset the "Z" to 0
-      if (index === last) {
-        // setTimeout(() => {
-        //   // element.style.transform = `translate3d(${x}px, -5px, 0px) rotate(${degrees}deg)`;
-        // }, 50);
-        //
-
-        // element.style.transform = `translate3d(${x}px, 0px, 0px) rotate(${degrees}deg)`;
-        // element.style.zIndex = 0;
-
-        /* Keyframe like */
-        // element.style.transformOrigin = "50% 0";
-        // element.style.transform = `translate3d(${x}px, -30px, 0px) rotate(${degrees}deg) scale(0.90)`;
-        // setTimeout(() => {
-        // }, 20);
-
-        setTimeout(() => {
-          element.style.zIndex = 0;
-          // element.style.transitionDuration = "300ms";
-        }, 1);
-
-        setTimeout(() => {
-          element.style.transform = `translate3d(${x}px, 0px, 0px) rotate(${degrees}deg) scale(1)`;
-          element.style.transformOrigin = "";
-          element.style.zIndex = 0;
-          // element.style.transitionDuration = "50ms";
-        }, 50);
-      } else {
-        element.style.transitionDuration = "300ms";
-        element.style.transform = `translate3d(${x}px, ${newY}px, ${newZ}px) rotate(${degrees}deg) scale(1)`;
-      }
-
-      setTimeout(() => (element.style.transitionDuration = "0ms"), 300);
-    });
-  }
-
-  /**
-   * @callback sortPhotosCallback
-   * @param {maxLength} number
-   * @param {number} index
-   * @returns {number}
-   */
-
-  /**
-   *
-   * @param {NodeListOf<Element>} elms
-   * @param {sortPhotosCallback} cb
-   */
-  function sortPhotos(elms, cb) {
-    if (elms.length === 0) return Array.from(elms);
-
-    const firstElement = elms[0];
-
-    const parent = firstElement.parentNode;
-    if (!parent) return Array.from(elms);
-
-    const last = elms.length - 1;
-    const elements = Array.from(elms)
-      .map((element, index) => {
-        const newIndex = cb(last, index);
-
-        return { element, newIndex };
-      })
-      .sort((a, b) => a.newIndex - b.newIndex)
-      .map(({ element }) => element);
-
-    elements.forEach((element, index) => {
-      element.remove();
-
-      // const preY = calculateYAxis(index, last, "UP");
-
-      if (index === last) {
-        element.style.transformOrigin = "50% 0";
-        element.style.transform = `translate3d(0px, -20px, -20px) rotate(0deg) scale(1)`;
-        element.style.zIndex = -2;
-        // element.style.transform = `translate3d(0px, 0px, 0px) rotate(0deg) scale(0.90)`;
-        element.style.transitionDuration = "150ms";
-      } else {
-        // element.style.transform = `translate3d(0px, ${preY / 2}px, 0px) rotate(0deg)`;
-      }
-
-      // element.style.transitionDuration = "1000ms";
-      parent.appendChild(element);
-    });
-
-    return elements;
-  }
-
-  /**
-   *
-   * @param {number} index
-   * @param {number} last
-   */
-  function calculateZAxis(index, last) {
-    const isMain = index === last;
-
-    return isMain ? 0 : -10 * (last - index);
-  }
-
-  /**
-   *
-   * @param {number} index
-   * @param {number} last
-   * @param {Direction} direction
-   */
-  function calculateYAxis(index, last, direction) {
-    const isMain = index === last;
-
-    if (direction === "DOWN") {
-      return isMain ? 0 : 20 * (last - index);
-    }
-
-    return isMain ? 0 : -20 * (last - index);
   }
 
   /**
