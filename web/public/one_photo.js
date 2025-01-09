5fa7d754bdcccc099a76a9be47af83a1a1f5dfe3
Victor Quiroz <git@victorhqc.com>
2025-01-10 00:17:14 +0100

    fix: cleanup event on navigation

--- web/public/one_photo.js
+++ web/public/one_photo.js
@@ -1,18 +1,24 @@
 (function () {
   if (!window.__active_collection__) return;
 
-  const ac = window.__active_collection__;
-
   registerEsc();
 
   function registerEsc() {
-    document.addEventListener("keyup", (e) => {
+    const ac = window.__active_collection__;
+
+    const listener = (e) => {
       if (e.key === "Escape") {
-        htmx.ajax("GET", ac.ajax_path, {
-          source: `li[data-collection="${ac.name}"]`,
-          target: ".portfolio__photos",
-        });
+        htmx
+          .ajax("GET", ac.ajax_path, {
+            source: `li[data-collection="${ac.name}"]`,
+            target: ".portfolio__photos",
+          })
+          .then(() => {
+            document.removeEventListener("keyup", listener);
+          });
       }
-    });
+    };
+
+    document.addEventListener("keyup", listener);
   }
 })();
