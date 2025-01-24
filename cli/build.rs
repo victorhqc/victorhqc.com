68aa6077d784af372513ce76bd4c3bdf7156076e
Victor Quiroz <git@victorhqc.com>
2025-01-25 00:49:33 +0100

    Fix mobile home (#58)
    ## Description
    
    - Fix menu overlap
    - Add padding for photos on home
    - Change home title from index to home

---
+++ cli/build.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+fn main() {
+    let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    println!("cargo:rerun-if-changed=build.rs"); // Re-run build if build.rs changes
+    #[cfg(debug_assertions)]
+    let db_file = crate_dir.join("..").join("api").join("development.db");
+    #[cfg(not(debug_assertions))]
+    let db_file = crate_dir
+        .join("..")
+        .join("api")
+        .join("api_victorhqc_com.db");
+    let db_url = format!("sqlite:{}", db_file.display());
+    println!("cargo::rustc-env=DATABASE_URL={}", db_url);
+}
