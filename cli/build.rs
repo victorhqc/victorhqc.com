a8a129068c2d034fa5eb00d916a3db5fef54fbee
Victor Quiroz <git@victorhqc.com>
2025-01-26 17:20:36 +0100

    chore: dynamic aws keys
    This way I won't forget to update the keys when I upload or change my
    photos

--- cli/build.rs
+++ cli/build.rs
@@ -15,5 +15,33 @@ fn main() {
         .join("api")
         .join("api_victorhqc_com.db");
     let db_url = format!("sqlite:{}", db_file.display());
-    println!("cargo::rustc-env=DATABASE_URL={}", db_url);
+    println!("cargo:rustc-env=DATABASE_URL={}", db_url);
+
+    #[cfg(debug_assertions)]
+    {
+        build_aws_keys("DEVELOPMENT_");
+    }
+    #[cfg(not(debug_assertions))]
+    {
+        build_aws_keys("PRODUCTION_");
+    }
+}
+
+fn build_aws_keys(prefix: &str) {
+    let access_key =
+        std::env::var(format!("{}AWS_ACCESS_KEY_ID", prefix)).unwrap_or("".to_string());
+    println!("cargo:rustc-env=AWS_ACCESS_KEY_ID={}", access_key);
+
+    let secret_access_key =
+        std::env::var(format!("{}AWS_SECRET_ACCESS_KEY", prefix)).unwrap_or("".to_string());
+    println!(
+        "cargo:rustc-env=AWS_SECRET_ACCESS_KEY={}",
+        secret_access_key
+    );
+
+    let region = std::env::var(format!("{}AWS_REGION", prefix)).unwrap_or("".to_string());
+    println!("cargo:rustc-env=AWS_REGION={}", region);
+
+    let bucket_name = std::env::var(format!("{}AWS_BUCKET_NAME", prefix)).unwrap_or("".to_string());
+    println!("cargo:rustc-env=AWS_BUCKET_NAME={}", bucket_name);
 }
